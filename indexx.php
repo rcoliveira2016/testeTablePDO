23d4d72362de426f56e01efd182e7da68a3a58a5
rcoliveira2016 <roramon7@gmail.com>
2016-10-18 12:06:43 -0200

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,57 @@
+<?php
+	$b=new PDO("mysql:host=localhost;dbname=add", "root", "");
+	$nome=array(
+		"ramon",
+		"Luis",
+		"Paulo",
+		"Giovana",
+		"Mariana",
+		"Gustavo",
+		"Ana",
+		"Fabiula",
+		"Mauricio",
+		"Bruna",
+		"Rui",
+		"Cristiano",
+		"Edite",
+		"flordelico",
+		"Dorlico",
+		"Um Dois Tres de oliveira Quatro",
+		"Marcelo Piroka",
+		"Naira Pinto de Jesus",
+		"Fabiana",
+		"Fabio"
+	);
+	$p=array(
+		"Arroz",
+		"Games",
+		"ticTac",
+		"Maconha",
+		"Cristal",
+		"Batata",
+		"Margarina",
+		"Peito de frango",
+		"Crack",
+		"Netflix",
+		"PC"
+	);
+
+
+	echo "hhh";
+	for($i=0; $i < 200; $i++ ){
+		$unidade=rand(0, 500);
+		$d=rand(1, 31);
+		$d=($d < 10 )? "0$d" : $d;
+		$m=rand(1, 31);
+		$m=($m < 10 )? "0$m" : $m;
+		$data=$d."/".$m."/".rand(1970, 2016);
+		$insert=$b->prepare("INSERT INTO `usuario`(`nome`, `unidade`, `produto`, `data`) VALUES (?,?,?,?)");
+		$insert->bindParam(1, $nome[rand(0,18)]);
+		$insert->bindParam(2, $unidade);
+		$insert->bindParam(3, $p[rand(0,10)]);
+		$insert->bindParam(4, $data);
+		$insert->execute();
+		echo "$i<br>";
+	}
+
+?>
--- table.php
+++ table.php
@@ -1,5 +1,20 @@
+<?php
+  $b=new PDO("mysql:host=localhost;dbname=add", "root", "");
+  $rs=$b->query("select * from usuario");
+  $num_total=$rs->rowCount();
+  $num_por_pag=10;
+  $pag=(isset($_GET['pag']) and !empty($_GET['pag']) and $_GET['pag']!=0)? $_GET['pag'] : 1;
+  $init=$num_por_pag*$pag-$num_por_pag;
+  $sql="select * from usuario LIMIT $init, $num_por_pag";
+  $rs=$b->query($sql);
+
+
+?>
 <table border="1">
   <tr>
+    <td>
+      id
+    </td>
     <td>
       Nome
     </td>
@@ -15,11 +30,9 @@
   </tr>
 <?php
 
-
-  $b=new PDO("mysql:host=localhost;dbname=add", "root", "");
-  $rs=$b->query("select * from usuario order by data");
   while ($row= $rs->fetch(PDO::FETCH_OBJ)) {
     echo "<tr>";
+    echo "  <td>$row->id</td>";
     echo "  <td>$row->nome</td>";
     echo "  <td>$row->unidade</td>";
     echo "  <td>$row->produto</td>";
@@ -30,15 +43,16 @@
  ?>
 </table>
 <?php
-  // quantidade total menos atual $rowCount-$rowAtual
-  $query=$b->query("select * from usuario ");
-  $cont=$query->rowCount();
-  $cont=($cont > 5)? ($cont/5) : false;
-  if ($cont) {
+  $count=$num_total/$num_por_pag;
+  if ($count>0) {
     echo "<p>";
-    for ($i=1; ($i < ($cont+1) and $i < 6) ; $i++) {
-      echo "<span style='border-ringht:solid 1px #000'>$i</span>";
+    echo "<span style='border:solid 1px #000;padding:10px'><a href='/src/table/pagina/1'><<</a></span>";
+    echo "<span style='border:solid 1px #000;padding:10px'><a href='/src/table/pagina/".((($pag-1)>0)? $pag-1 : 1)."'><</a></span>";
+    for ($i=$pag; ($i < ($count) and $i < ($pag+5)) ; $i++) {
+      echo "<span style='border:solid 1px #000;padding:10px'><a href='/src/table/pagina/$i'>$i</a></span>";
     }
+    echo "<span style='border:solid 1px #000;padding:10px'><a href='/src/table/pagina/".($pag+1)."'>></a></span>";
+    echo "<span style='border:solid 1px #000;padding:10px'><a href='/src/table/pagina/$count'>>></a></span>";
     echo "</p>";
   }
 
